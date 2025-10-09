cf03662022638389c10420d3122b954a2ffaa785
SANJAIKUMAR_S <sanjaisivakumar873@gmail.com>
2025-10-09 18:47:33 +0530

    Day 9 - Python Functions practice exercises

---
+++ day9_tuples.py
@@ -0,0 +1,15 @@
+# Day 9: Python Tuples
+
+fruits = ("apple", "banana", "cherry")
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(len(fruits))
+(a, b, c) = fruits
+print(a)
+print(b)
+print(c)
+
+numbers = (1, 2, 3, 2, 4)
+print(numbers.count(2))
+print(numbers.index(3))
